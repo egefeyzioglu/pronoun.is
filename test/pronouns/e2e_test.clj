133737d9380c9efe10d0f081581f6c16feadc166
Morgan Astra <morganastra@proton.me>
2026-05-07 21:40:44 -0700

    Add some visibility to subprocess in e2e test

--- test/pronouns/e2e_test.clj
+++ test/pronouns/e2e_test.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer [deftest testing is]]
             [clj-http.lite.client :as client]
             ;; [clojure.edn :as edn]
+            [clojure.tools.logging :as log]
             [clojure.java.shell :refer [sh]]
             [clojure.java.process :as p]
             [clojure.java.io :as io]))
@@ -10,34 +11,42 @@
   (zero? (:exit (sh "fuser" (str port "/tcp")))))
 
 (defn find-open-port []
-  (loop []
-    (let [port (+ 1000 (rand-int 8999))]
-      (if-not (tcp-bound? port)
-        port
-        (recur)))))
+  (loop [port 3000]
+    (if-not (tcp-bound? port)
+      port
+      (recur (+ 1000 (rand-int 8999))))))
 
-(defn wait-for-server [timeout-ms port]
+(defn wait-for-server [timeout-ms port sproc]
   (let [start-time (System/currentTimeMillis)
         exp-time (+ timeout-ms start-time)]
+
+    (log/info "Waiting for server to start on" port start-time exp-time)
     (loop []
       (when (> (System/currentTimeMillis) exp-time)
-        (throw (ex-info "Server startup timeout" {})))
+        (with-open [log-reader (-> (p/stderr sproc)
+                                   io/reader)]
+          (let [log-lines (take 4 (line-seq log-reader))]
+            (log/warn log-lines)
+            (throw (ex-info "Server startup timeout" {})))))
       (Thread/sleep 500)
       (if (tcp-bound? port)
-        true
+        (do (log/info "Server subprocess took"
+                      (- (System/currentTimeMillis) start-time)
+                      "ms to bind port")
+            true)
         (recur)))))
 
-(defn server [port]
-  (p/start {:clear-env true
-            :env {"PORT" (str port)}}
+(defn start-server [port]
+  (p/start {:env {"PORT" (str port)
+                  "CLASSPATH" ""}}
            "lein" "run"))
 
 (deftest ^:e2e e2e-server-test
-  (let [maxwait 60000
+  (let [maxwait 120000
         port (find-open-port)
-        sproc (server port)]
+        sproc (start-server port)]
 
-    (wait-for-server maxwait port)
+    (wait-for-server maxwait port sproc)
 
     (testing (str "Boot server " port " and get front page"))
     (let [response (client/get (str "http://localhost:" port)
@@ -54,8 +63,8 @@
                           (slurp log-reader)))]
 
         (sh "fuser" "-k" (str port "/tcp"))
+        (log/info "*** Subprocess log follows ***\n" @final-log)
         (is (re-find #":request-method :get, :uri \"/\"" @final-log))
         (is (not (re-find #"ERROR" @final-log))))))
+
   (shutdown-agents))
-
-
